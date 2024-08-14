9d0fb5c2f5405e1aeb4e26e8e6294409ba00a29e
Sidak <magicalsidak@gmail.com>
2024-08-14 16:40:54 -0400

    everything should work now

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
   console.log(process.env.MONGO_URI);
   try {
-    await mongoose.connect(process.env.mongo_uri, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.js";
 import dummyRoutes from "./routes/dummy.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+// console.log(process.env);
 
 const app = express();
 console.log("running server");
@@ -14,7 +18,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api", authRoutes);
-app.use("/api", dummyRoutes);
+// app.use("/api", dummyRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
